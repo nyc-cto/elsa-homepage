58595e1151662be2705a4b9c6755e39f73abfd98
Rapi Castillo <racastillo@cto.nyc.gov>
2021-01-08 14:54:31 -0500

    Aesthetic Fixes

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -66,7 +66,7 @@ export default function Home(props) {
           {t('fullReportDescription')}
         </Paragraph>
         <br/>
-        <Button link="/DOH_Unity_Project_Workforce_Manual.pdf"
+        <Button link="https://github.com/nyc-cto/tms"
           onClick={()=> logEvent("Button Click", "User downloaded report")}>
             {t('downloadReport')}
         </Button>
